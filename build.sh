3e7494b98f958e99233eba95e46b338b8a322054
Orgad Shaneh <orgad.shaneh@audiocodes.com>
2024-07-09 10:59:05 +0000

    Fix build
    Issue: VAG-2083
    Change-Id: Ib4f2dba3f95e3c6a625bcba5d1ff00019bbfd10b

--- build.sh
+++ build.sh
@@ -5,5 +5,6 @@ set -ex
 npm config set @ac-saas:registry https://artifactory.corp.audiocodes.com/artifactory/api/npm/saas-infra-npm-local
 
 export PACKAGE_NAME=drachtio-siprec
+export NO_TESTS=1
 
 npx -y -p @ac-saas/byoc-build@latest docker-build "$@"
